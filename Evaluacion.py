54a9ae441d5556e3d1227ed55bc9b3e52f756590
Alex-DUoc <alex.ibanez@duocuc.cl>
2024-04-09 01:32:00 +0000

    Evaluación N°1 DRY7122

---
+++ Evaluacion.py
@@ -0,0 +1,3 @@
+print("Evaluación N°1 Programación y Redes Virtualizadas")
+print("Alex Ibañez y Solange Caceres")
+
---
+++ datos.py
@@ -0,0 +1,9 @@
+vNom = str(input("Ingrese su nombre: "))
+vApe = str(input("Ingrese su Apellido: "))
+vSec = str(input("Ingrese su Sección: "))
+vSed = str(input("Ingrese su Sede: "))
+
+print("Nombre: ", vNom)
+print("Apellido: ", vApe)
+print("Sección: ", vSec)
+print("SEde: ", vSed)
